bbc7bc19301d7908dc318173ea9d98f9e5a808f2
Sander-317 <80258328+Sander-317@users.noreply.github.com>
2021-04-08 14:51:51 +0200

    Add files via upload

---
+++ opdracht_021_if_else_statment/script.js
@@ -0,0 +1,22 @@
+const age = 18;
+const isFemale = true;
+const driverStatus = "bob0";
+
+if (age > 17) {
+    console.log("je mag naar binnen");
+}
+else {
+    console.log("je mag niet naar binen");
+}
+if (isFemale == true) {
+    console.log("je bent een vrouw");
+}
+else {
+    console.log("je bent een man");
+}
+if (driverStatus == "bob") {
+    console.log("je bent de bob je mag rijden")
+}
+else {
+    console.log("je mag niet rijden")
+}
